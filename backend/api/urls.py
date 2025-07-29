6d51fc0d810589fe7bc75ee543f449f9742a72f0
visheshsri20 <77776404+visheshsri20@users.noreply.github.com>
2025-07-29 14:41:26 +0530

    Updated API endpoints for better understanding

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/',views.NoteListCreate.as_view(),name='note-list'),
+    path('notes/read/', views.NoteList.as_view(), name='note-list'),
+    path('notes/create/', views.NoteCreate.as_view(), name='note-create'),
     path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='delete-note'),
     path('notes/update/<int:pk>/', views.NoteUpdate.as_view(), name='update-note'),
 ]
